7202208fa7abdf700b068fe6606b0bb7e1576d60
tomiwolf <tomiwolfman@gmail.com>
2022-10-13 00:14:26 -0300

    started routing

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,14 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const pokemon  = require('./pokemon');
+const types = require('./types');
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.use('/pokemon', pokemon);
+router.use('/types', types);
 
 module.exports = router;
