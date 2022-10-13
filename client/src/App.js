eb112ef5e21b0861abcad879456a888444370893
tomiwolf <tomiwolfman@gmail.com>
2022-10-13 01:33:04 -0300

    added renders

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
+import { Route, Switch } from 'react-router';
 import './App.css';
+import Form from './components/Form/Form';
+import Home from './components/Home/Home';
+import CardDetail from './components/CardDetail/CardDetail';
+import LandingPage from './components/LandingPage/LandingPage';
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Pokemon</h1>
+    <Switch>
+      <Route exact path='/' component={LandingPage} />
+      <Route exact path='/home' component={Home} />
+      <Route path='/form' component={Form} />
+      <Route path='/detail/:id/:flagId' component={CardDetail} />
+    </Switch>
     </div>
   );
 }
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,75 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+import React from "react";
+import { configure, shallow } from "enzyme";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import { MemoryRouter } from "react-router-dom";
+import configureStore from "redux-mock-store";
+import { Provider } from "react-redux";
+
+import Home from "./components/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
+import Cards from "./components/Cards/Cards";
+
+configure({ adapter: new Adapter() });
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+
+describe("App", () => {
+  let store;
+  const middlewares = [];
+  const mockStore = configureStore(middlewares);
+
+  beforeEach(() => {
+    store = mockStore({
+      pokemonList: [],
+      pokemonFiltered: [],
+      pokeDetail: {},
+      pokemonTypes: [],
+      loading: false,
+      pokemonNotFound: false,
+    });
+  });
+
+  describe("El componente Nav debe renderizar en todas las rutas.", () => {
+    it('Debería renderizarse en la ruta "/"', () => {
+      const wrapper = mount(
+        <Provider store={store}>
+          <MemoryRouter initialEntries={["/"]}>
+            <App />
+          </MemoryRouter>
+        </Provider>
+      );
+      expect(wrapper.find(Navbar)).toHaveLength(1);
+    });
+    it('Debería renderizarse en la ruta "/Navbar"', () => {
+      const wrapper = mount(
+        <Provider store={store}>
+          <MemoryRouter initialEntries={["/Navbar"]}>
+            <App />
+          </MemoryRouter>
+        </Provider>
+      );
+      expect(wrapper.find(Nav)).toHaveLength(1);
+    });
+  });
+
+  it('El componente Home debe renderizar en la ruta /home (Sólo en la ruta "/home")', () => {
+    const wrapper = mount(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={["/home"]}>
+          <App />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    expect(wrapper.find(Home)).toHaveLength(1);
+  });
 });
